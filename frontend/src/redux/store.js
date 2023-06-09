49e7f74702afed4dc701a8a326dc8a4319b4ca97
SC-Sun <shangchisun@gmail.com>
2023-06-09 16:47:44 +0200

    created productActions & productReducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,15 @@
 import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { cartReducer } from "./reducers/cartReducders";
+import {
+  getProductsReducer,
+  getProductDetailsReducer,
+} from "./reducers/productReducers";
 
 const reducer = combineReducers({
   cart: cartReducer,
+  getProducts: getProductsReducer,
+  getProductDetails: getProductDetailsReducer,
 });
 
 const store = configureStore({ reducer: { reducer } });
